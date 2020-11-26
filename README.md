2472e15b9858dc4b5a19a2a313418e8458fa0451
Carlos Henrique <carlos.silva@novaandradina.org>
2020-11-26 14:06:00 -0300

    initial commit

--- README.md
+++ README.md
@@ -1,1 +1,23 @@
 # signed-payload-dynamic-qrcode
+Study repo for payload signing according to BACEN Pix dinamyc qr code requirements
+
+To generate a public/private key pair run
+
+```
+openssl req -x509 -sha256 -nodes -days 365 -newkey rsa:2048 -outform pem -keyout certs/privateKey.key -out certs/certificate.crt
+```
+
+To start the application
+
+```
+npm start
+```
+
+Endpoints 
+> POST /payload/
+
+> GET /payload/:id
+
+> GET /jwk
+
+This was created just for JWS/JWK/JOSE study purposes
---
+++ index.js
@@ -0,0 +1,20 @@
+const jsonServer = require('json-server');
+const path = require('path');
+
+const server = jsonServer.create();
+const middlewares = jsonServer.defaults();
+const router = jsonServer.router(path.join(__dirname, 'db.json'));
+const { overwriteRender, getJwkset } = require('./payload');
+const { keystore } = require('./keystore');
+
+server.use(jsonServer.bodyParser);
+server.use(middlewares);
+server.get('/jwk', getJwkset);
+
+server.use(router);
+
+router.render = overwriteRender;
+
+server.listen(3000, () => {
+  console.log('JSON Server is running');
+});
